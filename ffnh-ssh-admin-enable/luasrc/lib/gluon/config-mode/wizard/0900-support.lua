577d3ea28d5923aef06927025ddb53fdb9ba16bc
Michael H. <35325905+ddreamer75@users.noreply.github.com>
2025-01-25 21:32:33 +0100

    Inital relrase

---
+++ ffnh-ssh-admin-enable/luasrc/lib/gluon/config-mode/wizard/0900-support.lua
@@ -0,0 +1,18 @@
+return function(form, uci)
+	local platform = require 'gluon.platform'
+
+  local pkg_i18n = i18n 'ffnh-ssh-admin-enable'
+
+  local section = form:section(Section, nil, pkg_i18n.translate(
+		"Allow remote maintenance or support from Freifunk-Nordhessen e.V. "
+      .. "Activation gives our administrators access to your node"
+	))
+
+  local ssh = section:option(Flag, 'enabled', pkg_i18n.translate("Enable support"))
+  ssh.default = uci:get_bool('ffnh-ssh-manager', 'settings', 'enabled')
+
+  function ssh:write(data)
+    uci:set('ffnh-ssh-manager', 'settings', 'enabled', data)
+    uci:save('ffnh-ssh-manager')
+  end
+end
